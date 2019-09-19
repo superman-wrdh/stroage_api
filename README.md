b57062dc2d2870240f657a0f165b444b8b20331a
superman-wrdh <13419655360@163.com>
2019-09-19 10:25:10 +0800

    update doc

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
+go get github.com/astaxie/beego
+go get github.com/google/uuid
+go get github.com/beego/bee
+go get github.com/astaxie/beego/toolbox
 
 bee run -gendoc=true -downdoc=true
+
--- controllers/file.go
+++ controllers/file.go
@@ -3,11 +3,12 @@ package controllers
 import (
 	"fmt"
 	"github.com/astaxie/beego"
-	"go-common/app/service/main/member/model"
+	//"go-common/app/service/main/member/model"
 	"io/ioutil"
 	"os"
 	"path"
-	"strings"
+	//"strings"
+	"github.com/google/uuid"
 )
 
 // Operations about Users
@@ -22,8 +23,9 @@ type FileController struct {
 // @Failure 403 body is empty
 // @router / [post]
 func (u *FileController) Post() {
-	uid := model.UUID4()
-	uid = strings.Replace(uid, "-", "", -1)
+	uuid, _ := uuid.NewUUID()
+	uid := uuid.String()
+	//uid = strings.Replace(uid, "-", "", -1)
 	file, header, _ := u.GetFile("file")
 	ContentType := header.Header.Get("Content-Type")
 	FileSize := header.Size
